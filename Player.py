81189876057d2b52e62f4562a2158e661b2864f3
isung <isung@cs.wisc.edu>
2015-09-22 12:31:01 -0500

    Initial creation of files.

---
+++ Player.py
@@ -0,0 +1,7 @@
+"""
+Player object holds the information about the player, such as number of units controlled,
+territories owned, turn status, etc.
+"""
+
+__author__ = 'isung'
+
---
+++ Unit.py
@@ -0,0 +1,7 @@
+"""
+Units are the basic fighting unit in the game. They have several attributes such as unit type,
+location on grid, health, etc.
+"""
+
+__author__ = 'isung'
+
